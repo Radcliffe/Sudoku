45c2b94c1d378ad92f77b8f62b3c0bcd3ff4c8f1
David Radcliffe <dradcliffe@gmail.com>
2019-12-18 12:30:02 -0600

    input validation

--- app/Http/Controllers/SudokuController.php
+++ app/Http/Controllers/SudokuController.php
@@ -8,11 +8,49 @@ use Illuminate\Http\Request;
 
 class SudokuController extends Controller {
     public function showOneSudoku(Request $request) {
+        $size = $request->get('size', 9);
+        if ($size != '9') {
+            return response()->json(['error' => 'The value of \'size\' must be 9. Other values are not supported yet.']);
+        }
+
         if ($request->has('id')) {
-            $id = $request->get('id');
+            $id = (int) $request->get('id');
+            if ($id < 1 || $id > 1000000) {
+                return response()->json(['error' => 'The value of \'id\' must be an integer between 1 and 1000000 inclusive.']);
+            }
         } else {
             $id = rand(1, 1000000);
         }
-        return response()->json(Sudoku::find($id));
+
+        $show_puzzle = $request->get('puzzle', '1');
+        $show_solution = $request->get('solution', '1');
+        $sparse = $request->get('sparse', '0');
+
+        $sudoku = Sudoku::find($id);
+        if ($sparse == '1') {
+            $puzzle = [];
+            $solution = [];
+            $strlen = strlen($sudoku['solution']);
+            for ($i = 0; $i < $strlen; $i++) {
+                $cell = [
+                    'x' => $i % $size,
+                    'y' => (int) ($i / $size),
+                    'value' => (int) $sudoku['solution'][$i],
+                ];
+                $solution[] = $cell;
+                if ($sudoku['puzzle'][$i] != 0) {
+                    $puzzle[] = $cell;
+                }
+            }
+            $sudoku['puzzle'] = $puzzle;
+            $sudoku['solution'] = $solution;
+        }
+        if ($show_puzzle == '0') {
+            unset($sudoku['puzzle']);
+        }
+        if ($show_solution == '0') {
+            unset($sudoku['solution']);
+        }
+        return response()->json($sudoku);
     }
 }
