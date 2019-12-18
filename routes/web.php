d5a521359ff064c84eaf10cfbdf91fc26608cd42
David Radcliffe <dradcliffe@gmail.com>
2019-12-18 12:54:18 -0600

    remove comments

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,6 @@
 |--------------------------------------------------------------------------
 | Application Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
 */
 
 $router->get('/', function () use ($router) {
@@ -17,10 +12,4 @@ $router->get('/', function () use ($router) {
 
 $router->group(['prefix' => 'api'], function () use ($router) {
     $router->get('sudoku', ['uses' => 'SudokuController@showOneSudoku']);
-
-    // $router->post('authors', ['uses' => 'AuthorController@create']);
-
-    // $router->delete('authors/{id}', ['uses' => 'AuthorController@delete']);
-
-    // $router->put('authors/{id}', ['uses' => 'AuthorController@update']);
 });
